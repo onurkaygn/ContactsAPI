68ff4c746410acb7417060a7ed29ea81838b11d2
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2023-12-17 17:02:28 +0300

    POST added

--- ContactsAPI/Controllers/ContactsController.cs
+++ ContactsAPI/Controllers/ContactsController.cs
@@ -1,6 +1,6 @@
 ﻿using ContactsAPI.Data;
 using Microsoft.AspNetCore.Mvc;
-
+using ContactsAPI.Models;
 namespace ContactsAPI.Controllers
 {
     [ApiController]
@@ -13,9 +13,25 @@ namespace ContactsAPI.Controllers
             this.dbContext = dbContext;
         }
         [HttpGet]
-        public IActionResult GetContacts()
+        public async Task<IActionResult> GetContacts()
         {
            return Ok(dbContext.Contacts.ToList());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var newContact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                FullName = addContactRequest.FullName,
+                Phone = addContactRequest.Phone,
+            };
+            await dbContext.Contacts.AddAsync(newContact);
+            await dbContext.SaveChangesAsync();
+            return Ok(newContact);
         }
     }
 }
