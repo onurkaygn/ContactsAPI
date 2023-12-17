fe9d0324c0ca89c7b10e4e6c427dc5a9006be595
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2023-12-17 16:49:55 +0300

    GET added

---
+++ ContactsAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactsAPI.Models;
+
+namespace ContactsAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
